fa30b60b25e72e7302850b0987ee3d49763a291a
Jardel <dedi27@gmail.com>
2021-10-10 22:11:17 -0300

    criação do module

---
+++ servers/variables.tf
@@ -0,0 +1,10 @@
+variable "image_id" {
+  default = "ami-12345678"
+  type = string
+  description = "The id of the machine image (AMI) to use for the server."
+
+  validation {
+    condition = length(var.image_id) > 4 && substr(var.image_id, 0, 4) == "ami-"
+    error_message = "The image id must be a valid AMI id, starting with \"ami-\"." 
+  }
+}
